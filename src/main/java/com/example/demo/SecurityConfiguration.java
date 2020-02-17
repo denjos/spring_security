e9c4f57a3a6eaadb473ce963a886d8a4f8f40fd5
unknown <oltursa.developer@gmail.com>
2020-02-16 20:44:49 -0500

    security jdbc

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,37 +1,50 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
-	
+
+	private DataSource ds;
+
+	SecurityConfiguration(DataSource ds){
+		this.ds=ds;
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth.inMemoryAuthentication()
-		.withUser("oscar").password("oscar").roles("USER")
-		.and()
-		.withUser("javier").password("javier").roles("ADMIN");
+		auth.jdbcAuthentication()
+				.dataSource(ds)
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username,authority from authorities where username=?")
+				.passwordEncoder(passwordEncoder());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/admin").hasAnyRole("ADMIN")
-			.antMatchers("/user").hasAnyRole("USER")
-			.antMatchers("/**").authenticated()
-			.and().formLogin();
+				.antMatchers("/admin").hasRole("ADMIN")
+				.antMatchers("/user").hasRole("USER")
+				.antMatchers("/").authenticated()
+				.and().formLogin();
 	}
-	
+
 	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	public PasswordEncoder passwordEncoder() {
+	    PasswordEncoder encoder = new BCryptPasswordEncoder();
+	    return encoder;
 	}
-	
 }
